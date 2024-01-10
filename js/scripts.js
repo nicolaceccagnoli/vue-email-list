21ae3b060c2ef0edae634196100c1c881c8c79eb
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2024-01-10 17:41:20 +0100

    Add Transition and Fixed Mounted() instance

--- js/scripts.js
+++ js/scripts.js
@@ -8,24 +8,31 @@ createApp({
             message: 'Vue Email List',
 
             // Creo un array vuoto che andrà a contenere le email generate randomicamente
-            myRandomEmail: []
+            myRandomEmail: [],
+            nMail: 10
         };
     },
     mounted() {
 
         // Creo un ciclo che vada a richiamare 10 volte le chiamate asincrone della libreria di Axios 
-        for (let i = 0; i <  10; i++) {
+        for (let i = 0; i <  this.nMail; i++) {
             // Richiamo la libreria di axios
             axios
-            .get('https://flynn.boolean.careers/exercises/api/random/mail')  // Tramite la proprietà .get richiamo l'API
-            .then((res) => {                                                 // Tramite la proprietà .then chiedo alla funzione di estrapolare la risposta e pusharla dentro l'array delle mail 
-                let newRandomEmail = res.data.response;
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')   // Tramite la proprietà .get richiamo l'API
+            .then(res => res.data)                                            // Tramite la proprietà .then chiedo alla funzione di estrapolare la risposta e pusharla dentro l'array delle mail 
+            let newRandomEmail = data.response;                               // Posso Concatenare più volte la proprietà .then 
+            .then((data) => {                                                 
                 this.myRandomEmail.push(newRandomEmail);
 
+                console.log('new RandomEmail: ', newRandomEmail);
+                
             })
+            // .catch(err => console.log('Error: ', err))
         }
 
-        console.log(this.myRandomEmail);
+        // this.loaded = true
+
+        console.log('this.myRandomEmail: ', this.myRandomEmail);
 
     }
   // Monto l'istanza di Vue in pagina
