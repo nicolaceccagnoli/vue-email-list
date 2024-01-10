3b73ce0c0f9f62ec8988567a96487e654a1b3996
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2024-01-10 15:07:22 +0100

    Made API call w/axios

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,27 @@ createApp({
     data() {
         return {
             message: 'Vue Email List',
+
+            // Creo un array vuoto che andrà a contenere le email generate randomicamente
+            myRandomEmail: []
         };
+    },
+    mounted() {
+
+        // Creo un ciclo che vada a richiamare 10 volte le chiamate asincrone della libreria di Axios 
+        for (let i = 0; i <  10; i++) {
+            // Richiamo la libreria di axios
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')  // Tramite la proprietà .get richiamo l'API
+            .then((res) => {                                                 // Tramite la proprietà .then chiedo alla funzione di estrapolare la risposta e pusharla dentro l'array delle mail 
+                let newRandomEmail = res.data.response;
+                this.myRandomEmail.push(newRandomEmail);
+
+            })
+        }
+
+        console.log(this.myRandomEmail);
+
     }
   // Monto l'istanza di Vue in pagina
 }).mount('#app');
